d87a8239e9508b60925c0d44370f1744fd55cb9f
Rodolfo Andino <aandino186@gmail.com>
2024-10-13 14:08:11 -0600

    Restructure of site

---
+++ src/i18n/ui.ts
@@ -0,0 +1,25 @@
+export const languages = {
+    en: 'English',
+    es: 'Español',
+  };
+  
+  export const defaultLang = 'en';
+  
+  export const ui = {
+    en: {
+      'nav.home': 'Home',
+      'nav.about': 'About',
+      'nav.blog': 'Blog',
+      'nav.lang.screen-reader': 'Select you language',
+      'nav.lang.english': 'English',
+      'nav.lang.spanish': 'Spanish',
+    },
+    es: {
+      'nav.home': 'Inicio',
+      'nav.about': 'Sobre mi',
+      'nav.blog': 'Blog',
+      'nav.lang.screen-reader': 'Selecciona tu lenguage',
+      'nav.lang.english': 'Ingles',
+      'nav.lang.spanish': 'Español',
+    },
+  } as const;
